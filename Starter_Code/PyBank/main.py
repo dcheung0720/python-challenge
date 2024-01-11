29d92569ab0b16ff91bb4cbf7da4673f55a542ee
David <davidcheung231@gmail.com>
2024-01-11 13:06:06 -0600

    finished part 1

--- Starter_Code/PyBank/main.py
+++ Starter_Code/PyBank/main.py
@@ -13,7 +13,9 @@ with open(csv_path) as csv_file:
 
     # variables set up for write later
     total_months =  0
-    total_reserve = 0
+    total_profit = 0
+    average_Change = 0
+    previous_profit = 0
     #assume worst case for greatest increase and decrease
     greatest_increase_date = ""
     greatest_increase = float('-inf')
@@ -30,23 +32,32 @@ with open(csv_path) as csv_file:
         #counter months
         total_months += 1
 
-        #total_reserve
-        total_reserve += netProfit
+        #total_profit
+        total_profit += netProfit
 
-        # brute force search of min (since we are already looping, otherwise we can use min for optimization)
-        if netProfit < greatest_decrease:
+        #change in profit
+        delta_profit = netProfit - previous_profit
+
+        #average_Change_sum, only check the previous value if the index is > 1
+        if(total_months > 1):
+            average_Change += delta_profit
+
+        # brute force search of min 
+        if delta_profit < greatest_decrease:
             greatest_decrease_date = date
-            greatest_decrease = netProfit
+            greatest_decrease = delta_profit
         
-        #brute force search of max (since we are already looping, otherwise we can use max for optimization)
-        if netProfit > greatest_increase:
+        #brute force search of max
+        if delta_profit > greatest_increase:
             greatest_increase_date = date
-            greatest_increase = netProfit
+            greatest_increase = delta_profit
 
-    #write file
+        previous_profit = netProfit
+
+    #write file path
     analysis_path = os.path.join("Analysis", "analysis.txt")
 
-    average_Change = total_reserve/ total_months
+    average_Change /= (total_months - 1)
 
     # open analysis file
     with open(analysis_path, 'w') as analysis_file:
@@ -54,11 +65,12 @@ with open(csv_path) as csv_file:
         lines = ['Finanial Analysis\n',
                  "--------------------\n",
                  f"Total Months: {total_months}\n",
-                 f"Total: {total_reserve}\n",
-                 f"Average Change: {average_Change}\n",
+                 f"Total: ${total_profit}\n",
+                 f"Average Change: ${round(average_Change, 2)}\n",
                  f"Greatest Increase in Profits: {greatest_increase_date} (${greatest_increase})\n",
                  f"Greatest Decrease in Profits: {greatest_decrease_date} (${greatest_decrease})\n" ]
-
+        
+        #write the lines
         analysis_file.writelines(lines)
 
 
